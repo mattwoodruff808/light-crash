904aad2ce2aeea3e848da36e381cc9675b24a804
mattwoodruff808 <mjwoodruff93@gmail.com>
2021-07-21 21:29:31 -0600

    Added, then fixed, line colliders

--- main.lua
+++ main.lua
@@ -5,7 +5,7 @@ function love.load()
     love.window.setMode(1400, 900)
 
     sprites = {}
-    sprites.redShip = love.graphics.newImage('sprites/red-ship.png')
+    sprites.redShip = love.graphics.newImage('sprites/red-ship30.png')
 
     wf = require 'libraries/windfield/windfield'
     world = wf.newWorld(0, 0, false)
@@ -14,22 +14,27 @@ function love.load()
     world:addCollisionClass('PlayerOne')
     world:addCollisionClass('Trail')
 
-    playerSizeX = 15
-    playerSizeY = 15
+    local playerSizeX = 25
+    local playerSizeY = 25
     playerSpeed = 200
 
-    local p1StartX = 500
-    local p1StartY = 400
+    local p1StartX = 502
+    local p1StartY = 402
 
     playerOne = world:newRectangleCollider(p1StartX, p1StartY, playerSizeX, playerSizeY, {collision_class = 'PlayerOne'})
-    playerOne.x = 500
-    playerOne.y = 400
+    playerOne:setFixedRotation(true)
+    playerOne.x = 515
+    playerOne.y = 415
     playerOne.direction = "up"
-    playerOne.firstX = 507.5
-    playerOne.firstY = 407.5
+    playerOne.directionUpDown = 1
+    playerOne.directionLeftRight = 0
+    playerOne.radians = 0
+    playerOne.firstX = 515
+    playerOne.firstY = 415
     playerOne.lastX = 0
     playerOne.lastY = 0
     playerOne.lines = {}
+    playerOne.lineColliders = {}
 
     playerTwo = {}
 end
@@ -69,18 +74,30 @@ function love.draw()
     world:draw()
 
     love.graphics.setColor(1, 0.6, 0.6)
-    love.graphics.setLineWidth(9)
-    love.graphics.line(playerOne.firstX, playerOne.firstY, playerOne.x + 7.5, playerOne.y + 7.5)
+    love.graphics.setLineWidth(8)
+
+    if playerOne.direction == "right" then
+        love.graphics.line(playerOne.firstX, playerOne.firstY, playerOne.x - 15, playerOne.y)
+    end
+    if playerOne.direction == "left" then
+        love.graphics.line(playerOne.firstX, playerOne.firstY, playerOne.x + 15, playerOne.y)
+    end
+    if playerOne.direction == "up" then
+        love.graphics.line(playerOne.firstX, playerOne.firstY, playerOne.x, playerOne.y + 15)
+    end
+    if playerOne.direction == "down" then
+        love.graphics.line(playerOne.firstX, playerOne.firstY, playerOne.x, playerOne.y - 15)
+    end
 
     for i = 1, #playerOne.lines do
         local line = playerOne.lines[i]
 
         love.graphics.line(line[1], line[2], line[3], line[4])
-        love.graphics.circle("fill", line[3], line[4], 4.5)
+        love.graphics.circle("fill", line[3], line[4], 4)
     end
 
     love.graphics.setLineWidth(1)
-    love.graphics.draw(sprites.redShip, playerOne.x, playerOne.y)
+    love.graphics.draw(sprites.redShip, playerOne.x, playerOne.y, playerOne.radians, nil, nil, 15, 15)
 end
 
 --[[
@@ -89,24 +106,32 @@ end
 function love.keypressed(key)
     if key == "d" and playerOne.direction ~= "left" then
         playerOne.direction = "right"
+        playerOne.directionLeftRight = -1
+        playerOne.radians = math.pi/2
         drawPlayerLine()
         playerOne.firstX = playerOne.lastX
         playerOne.firstY = playerOne.lastY
     end
     if key == "a" and playerOne.direction ~= "right" then
         playerOne.direction = "left"
+        playerOne.directionLeftRight = 1
+        playerOne.radians = math.pi*3/2
         drawPlayerLine()
         playerOne.firstX = playerOne.lastX
         playerOne.firstY = playerOne.lastY
     end
     if key == "w" and playerOne.direction ~= "down" then
         playerOne.direction = "up"
+        playerOne.directionUpDown = 1
+        playerOne.radians = 0
         drawPlayerLine()
         playerOne.firstX = playerOne.lastX
         playerOne.firstY = playerOne.lastY
     end
     if key == "s" and playerOne.direction ~= "up" then
         playerOne.direction = "down"
+        playerOne.directionUpDown = -1
+        playerOne.radians = math.pi
         drawPlayerLine()
         playerOne.firstX = playerOne.lastX
         playerOne.firstY = playerOne.lastY
@@ -124,4 +149,21 @@ function drawPlayerLine()
 
     local line = {playerOne.firstX, playerOne.firstY, playerOne.lastX, playerOne.lastY}
     table.insert(playerOne.lines, line)
+
+    if playerOne.direction == "right" then
+        local lineCollider = world:newLineCollider(playerOne.firstX, playerOne.firstY, playerOne.lastX, playerOne.lastY + (22 * playerOne.directionUpDown), {collision_class = 'Trail'})
+        table.insert(playerOne.lineColliders, lineCollider)
+    end
+    if playerOne.direction == "left" then
+        local lineCollider = world:newLineCollider(playerOne.firstX, playerOne.firstY, playerOne.lastX, playerOne.lastY + (22 * playerOne.directionUpDown), {collision_class = 'Trail'})
+        table.insert(playerOne.lineColliders, lineCollider)
+    end
+    if playerOne.direction == "up" then
+        local lineCollider = world:newLineCollider(playerOne.firstX, playerOne.firstY, playerOne.lastX + (22 * playerOne.directionLeftRight), playerOne.lastY, {collision_class = 'Trail'})
+        table.insert(playerOne.lineColliders, lineCollider)
+    end
+    if playerOne.direction == "down" then
+        local lineCollider = world:newLineCollider(playerOne.firstX, playerOne.firstY, playerOne.lastX + (22 * playerOne.directionLeftRight), playerOne.lastY, {collision_class = 'Trail'})
+        table.insert(playerOne.lineColliders, lineCollider)
+    end
 end
